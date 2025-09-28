49aa022e71a029118ea0408bd6eadaf8d61bc078
대홍단감자 <lgh0005@naver.com>
2025-09-28 20:10:37 +0900

    Make socket non-blocking

--- ServerCore/DummyClient/DummyClient.cpp
+++ ServerCore/DummyClient/DummyClient.cpp
@@ -10,6 +10,11 @@ int main()
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 		return 1;
+
+	// I-1. 소켓 논블로킹화
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == SOCKET_ERROR)
+		return 1;
 	
 	// II. 연결할 서버의 IP 주소(지금은 루프백), 포트 번호 설정
 	SOCKADDR_IN serverAddr;
@@ -18,8 +23,18 @@ int main()
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777);
 
-	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-		return 1;
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			// 이건 이미 연결이 되었는데 또 연결을 요청할 경우에 대한 처리.
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+		}
+	}
 
 	cout << "Connected To Server!" << endl;
 
@@ -27,6 +42,7 @@ int main()
 	while (true)
 	{
 		// TODO
+		// 논블로킹 소켓인 경우, send/recv 모두 논블로킹으로 진행되므로 WSAEWOULDBLOCK 체크.
 	}
 
 	::closesocket(clientSocket);
--- ServerCore/Server/Server.cpp
+++ ServerCore/Server/Server.cpp
@@ -12,6 +12,15 @@ int main()
 	if (listenSocket == INVALID_SOCKET)
 		return 1;
 
+	// I-1. 소켓 논블로킹화 및 REUSEADDR 옵션 활성화
+	u_long on = 1;
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == SOCKET_ERROR)
+		return 1;
+
+	BOOL bOption = TRUE;
+	if (::setsockopt(listenSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&bOption, sizeof(BOOL)) == SOCKET_ERROR)
+		return 1;
+
 	// II. 주소/포트 번호 설정(bind)
 	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
@@ -35,10 +44,15 @@ int main()
 
 		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
 		if (clientSocket == INVALID_SOCKET)
-			return 1;
+		{
+			// IV-1. 논블로킹에 대해서 WSAEWOULDBLOCK의 경우 아직 클라가 오지 않았으므로 continue
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+		}
 
 		// V. 클라와 통신
 		// ex) 클라이언트의 IP 주소 출력
+		// 논블로킹 소켓인 경우, send/recv 모두 논블로킹으로 진행되므로 WSAEWOULDBLOCK 체크.
 		char ip[16];
 		::inet_ntop(AF_INET, &clientAddr.sin_addr, ip, sizeof(ip));
 		cout << "Client Connected! IP = " << ip << endl;
